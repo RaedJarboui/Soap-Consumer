c62f4478461dc8131b55e7afefda6bc0ed756eec
Raed Jarboui <raedjarboui1998@gmail.com>
2024-11-13 15:22:03 +0100

    consume soap webservice which contains soapAction

--- src/main/java/com/soap/consumer/config/MarshallerConfig.java
+++ src/main/java/com/soap/consumer/config/MarshallerConfig.java
@@ -1,21 +1,32 @@
 package com.soap.consumer.config;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-import com.soap.consumer.client.SoapClient;
+import com.soap.consumer.client.SOAPConnector;
 @Configuration
 public class MarshallerConfig {
 	
 	
 	//transform objects to xml using jaxb Marshaller
-	@Bean
-	public Jaxb2Marshaller marshaller() {
-		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		marshaller.setPackagesToScan("com.soap.consumer.calculator"); //scan classes from package path and bind them to xml
-		return marshaller;
-	}
+	 @Bean
+	    public Jaxb2Marshaller marshaller() {
+	        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+	       
+	        marshaller.setContextPath("com.soap.consumer.tempConvert");	// this is the package name specified in the <generatePackage> specified in pom.xml
+	        return marshaller;
+	    }
+
+	    @Bean
+	    public SOAPConnector soapConnector(Jaxb2Marshaller marshaller) {
+	        SOAPConnector client = new SOAPConnector();
+	        client.setDefaultUri("https://www.w3schools.com/xml/tempconvert.asmx?wsdl");
+	        client.setMarshaller(marshaller);
+	        client.setUnmarshaller(marshaller);
+	        return client;
+	    }
 	
 	
 }
--- src/main/java/com/soap/consumer/endpoint/SoapConsumerEndpoint.java
+++ src/main/java/com/soap/consumer/endpoint/SoapConsumerEndpoint.java
@@ -1,23 +1,37 @@
 package com.soap.consumer.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.soap.consumer.calculator.Add;
-import com.soap.consumer.calculator.AddResponse;
-import com.soap.consumer.client.SoapClient;
+import com.soap.consumer.client.SOAPConnector;
+import com.soap.consumer.tempConvert.CelsiusToFahrenheit;
+import com.soap.consumer.tempConvert.CelsiusToFahrenheitResponse;
+import com.soap.consumer.tempConvert.FahrenheitToCelsius;
+import com.soap.consumer.tempConvert.FahrenheitToCelsiusResponse;
 
 @RestController
 public class SoapConsumerEndpoint {
 	@Autowired 
-	private SoapClient service;
+	private SOAPConnector service;
 	
 	@PostMapping("/consumer/soap")
-	public AddResponse consumeSoapWebService(@RequestBody Add addRequest) {
-		return service.getAddResult(addRequest);
-		
+	public FahrenheitToCelsiusResponse consumeSoapWebService(@RequestBody FahrenheitToCelsius request) {
+		FahrenheitToCelsiusResponse response = (FahrenheitToCelsiusResponse) service.callWebService("https://www.w3schools.com/xml/FahrenheitToCelsius",request);
+        System.out.println("FahrenheitResult : " + response.getFahrenheitToCelsiusResult().toString());
+        return response;		
 	}
+	
+    @PostMapping("/getfahrenheight")
+    public CelsiusToFahrenheitResponse getFahrenheight(@RequestBody CelsiusToFahrenheit req){
+        System.out.println("input " + req.getCelsius());
+        CelsiusToFahrenheitResponse response = (CelsiusToFahrenheitResponse) service.callWebService("https://www.w3schools.com/xml/CelsiusToFahrenheit",req);
+        System.out.println("Got Response As below ========= : ");
+        System.out.println("FahrenheitResult : " + response.getCelsiusToFahrenheitResult().toString());
+        return response;
+
+    }
 
 }
